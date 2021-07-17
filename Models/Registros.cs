0c71b49ab81702a6828db9681fed5a15079b22c7
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2021-07-17 00:06:13 -0400

    Add project files.

---
+++ Models/Registros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Models
+{
+    public class Registros
+    {
+        public string Nickname { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        public string ConfirmarClave { get; set; }
+    }
+}
---
+++ Models/Usuarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Models
+{
+    public class Usuarios
+    {
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Clave { get; set; }
+    }
+}
